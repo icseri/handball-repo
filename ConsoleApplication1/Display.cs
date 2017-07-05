204a1ac0968f419003fb03eeb0caa0e04afe6621
tanulo14@athenepraxis.local <tanulo14@athenepraxis.local>
2017-07-05 20:37:02 +0200

    Display.cs elkészült

---
+++ ConsoleApplication1/Display.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+  static class Display
+  {
+    public static void DisplayPlayers(Player[] players)
+    {
+      foreach (var player in players)
+      {
+        Console.WriteLine($"Játékos: {player.Number}, Játkban töltött idő: {player.MinsOnPitch}");       
+      }
+    }
+  }
+}
