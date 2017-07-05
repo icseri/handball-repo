5dcda34e63f80a964050f2c7dc6795c370f8f146
kissbalazs@athenepraxispraxis.local <kissbalazs@athenepraxispraxis.local>
2017-07-05 18:34:18 +0200

    Player created

---
+++ ConsoleApplication1/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Player
+    {
+        public int Number { get; }
+        public bool IsOnPitch { get; set; }
+        public int MinsOnPitch { get; set; }
+    }
+}
