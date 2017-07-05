c78b95e7bf36f86c7b4b75c85792a1d046f24d4a
tanulo14@athenepraxis.local <tanulo14@athenepraxis.local>
2017-07-05 20:01:07 +0200

    Match függvények létrehozva

---
+++ ConsoleApplication1/Match.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+  class Match
+  {
+    readonly Player[] _players;
+    readonly Swap[] _swaps;
+
+
+    public Match(Player[] players, Swap[] swaps)
+    {
+      _players = players;
+      _swaps = swaps;
+    }
+
+    void PlayMatch()
+    {
+
+      for (int i = 1; i <= 60; i++)
+      {
+        Subs(i);
+        SetTime();
+      }
+    }
+
+    void Subs(int minute)
+    {
+      foreach (var swap in _swaps)
+      {
+        if (swap.Minutes == minute)
+        {
+          _players[swap.PlayerFrom - 1].IsOnPitch = false;
+          _players[swap.PlayerTo - 1].IsOnPitch = true;
+        }
+      }
+    }
+    
+    void SetTime()
+    {
+      foreach (var player in _players)
+      {
+        if (player.IsOnPitch)
+        {
+          player.MinsOnPitch++;          
+        }
+      }
+    }
+
+  }
+}
