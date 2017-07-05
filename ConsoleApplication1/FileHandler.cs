a3a7bcee9db6824b78a4ebac397814dd8b164f83
kissbalazs@athenepraxispraxis.local <kissbalazs@athenepraxispraxis.local>
2017-07-05 19:29:31 +0200

    Swap modositva, FileHandler letrehozva

---
+++ ConsoleApplication1/FileHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    static class FileHandler
+    {
+        const string SubFileName = "subs.txt";
+
+        public static Player[] CreatePlayers()
+        {
+            Player[] players = new Player[10];
+            for (int i = 0; i < 10; i++)
+            {
+                players[i] = new Player();
+                players[i].Number = i + 1;
+            }
+            return players;
+        }
+
+
+        public static Swap[] ReadSubs()
+        {
+            var data = File.ReadAllLines(SubFileName);
+            var subs = new Swap[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                subs[i] = Swap.FromString(data[i]);
+            }
+            return subs;
+        }
+
+    }
+}
--- ConsoleApplication1/Swap.cs
+++ ConsoleApplication1/Swap.cs
@@ -8,8 +8,26 @@ namespace ConsoleApplication1
 {
   class Swap
   {
-    public int Minutes { get; set; }
-    public int playerFrom { get; set; }
-    public int playerTo { get; set; }    
+        public Swap(int min, int playerFrom, int playerTo)
+        {
+            Minutes = min;
+            PlayerFrom = playerFrom;
+            PlayerTo = playerTo;
+        }
+
+        public static Swap FromString(string s)
+        {
+            var data = s.Split('.', ' ', 'p', 'e', 'r', 'c', ',');
+            return new Swap(
+                int.Parse(data[0]),
+                int.Parse(data[8]),
+                int.Parse(data[10])
+                );
+        }
+
+
+        public int Minutes { get; set; }
+        public int PlayerFrom { get; set; }
+        public int PlayerTo { get; set; }    
   }
 }
